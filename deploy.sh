e8870ea42ef41835a28f0bbdf41a5eca6d9370c3
Erik Jonasson <ejonasson@gmail.com>
2018-12-06 15:28:37 -0500

    Add Deploy Script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+touch .env
+echo "AMAZON_ACCESS_KEY_ID=$AMAZON_ACCESS_KEY_ID" >> ./.env
+echo "AMAZON_SECRET_ACCESS_KEY=$AMAZON_SECRET_ACCESS_KEY" >> ./.env
+echo "AMAZON_BUCKET_NAME=$AMAZON_BUCKET_NAME" >> ./.env
+nuxt generate
